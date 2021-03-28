ea7fe6af4c2d6f6c9a658d3b28b4ae00592d5f4b
Julia Ju <j7ju@uwaterloo.ca>
2021-03-28 04:23:18 -0400

    more updates

--- dotslogistics/src/components/navbar/Navbar.js
+++ dotslogistics/src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {MenuItems} from "./MenuItems"
 import './Navbar.css'
+import Dotslog from '../../pictures/dotslog.png'
+
 
 //use class since we are using states
 class Navbar extends Component {
@@ -16,7 +18,8 @@ class Navbar extends Component {
     render() {
         return(
             <nav className= "NavbarItems">
-                    <h1 className = "navbar-logo"> DotsLogistics <i className = "fab fa-react"></i></h1>
+                <img src = {Dotslog} style = {{width: '60px', paddingRight: '10px'}}></img>
+                    <h1 className = "navbar-logo"> DotsLogistics <i className = "fab fa-react" ></i></h1>
                     <div className = "menu-icon" onClick = {this.handleClick}>
                         <i className = {this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                     </div>
--- dotslogistics/src/pages/HomePage.jsx
+++ dotslogistics/src/pages/HomePage.jsx
@@ -5,13 +5,14 @@ import Features from "../components/Homepage/features/Features";
 import MoreInfo from  '../components/Homepage/more_info/MoreInfo'
 
 import Navbar from "../components/navbar/Navbar";
+import Navbar2 from "../components/navbar/Navbar2";
 
 
 
 function HomePage() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar2/>
       <HomeImage />
       <Features />
       <MoreInfo/>
