bee09f910b2592814fd6d93af3002db11d131b64
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-03-28 02:48:07 -0400

    merge ju and chris

--- dotslogistics/src/App.js
+++ dotslogistics/src/App.js
@@ -1,13 +1,44 @@
 import "./App.css";
+import React, { Component } from "react";
+
+//Import all needed Component for this tutorial
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+//pages
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
 import Marketplace from "./pages/Marketplace.js";
 import RequestQuote from "./pages/RequestQuote.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Marketplace />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          {/*All our Routes goes here!*/}
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/dash" component={Dashboard} />
+          <Route exact path="/404" component={NotFoundPage} />
+          <Route exact path="/marketplace" component={Marketplace} />
+          <Route
+            exact
+            path="/requestquote/:bus"
+            render={(props) => {
+              return <RequestQuote {...props} />;
+            }}
+          />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
